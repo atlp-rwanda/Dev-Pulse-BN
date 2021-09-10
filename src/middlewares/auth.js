8e1fe400c98525ec8366d4777be712f687350ec5
ceelogre <sam.nishimwe@yahoo.com>
2021-09-10 14:49:37 +0200

    fix: check for jwt secret

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,6 +5,10 @@ import Response from '../helpers/response';
 const verify = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
+    // if jwt secret is not defined, return
+    if (!process.env.JWT_SECRET) {
+      return Response.authenticationError(res, 'JWT secret is not defined');
+    }
     req.user = jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch (error) {
