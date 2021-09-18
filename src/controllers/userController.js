dbfda0c73ada15e99abaca0b680fc640b6f50897
mansurissa <mansurissa6@gmail.com>
2021-09-18 19:32:48 +0200

    chore(node version): update node version
    - update app to use node 12.x and above
    - added envVar of develop
    - update dev script to use seed:undo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,9 +5,8 @@ import UserService from '../services/userService';
 
 const { Op } = Sequelize;
 
-const {
-  findAllUsers, getEngineersByManager, getSingleEngineer, updateUser,
-} = UserService;
+const { findAllUsers, getEngineersByManager, getSingleEngineer, updateUser } =
+  UserService;
 
 /**
  * @class AuthController
@@ -23,22 +22,33 @@ class UserController {
    */
   static async updateRole(req, res) {
     try {
-      if (req.user.role !== 'Manager') return Response.authorizationError(res, 'You do not have access to perform this action');
+      if (req.user.role !== 'Manager')
+        return Response.authorizationError(
+          res,
+          'You do not have access to perform this action'
+        );
       const { email } = req.body;
       const check = await UserService.findOneUser({ email });
       if (!check) return Response.notFoundError(res, 'User not found');
-      if (check.role === 'Manager') return Response.badRequestError(res, 'The user is already an LF');
+      if (check.role === 'Manager')
+        return Response.badRequestError(res, 'The user is already an LF');
       const user = await UserService.updateUser({ role: 'LF' }, { email });
-      return Response.customResponse(res, 200, 'Successfully updated the user to LF', user[1][0]);
+      return Response.customResponse(
+        res,
+        200,
+        'Successfully updated the user to LF',
+        user[1][0]
+      );
     } catch (error) {
-      if (error.name === 'SequelizeValidationError') return Response.validationError(res, error.errors[0].message);
+      if (error.name === 'SequelizeValidationError')
+        return Response.validationError(res, error.errors[0].message);
       return Response.serverError(res, error);
     }
   }
 
   static async viewAllProfiles(req, res) {
-    let engineerIds; let
-      allUsers;
+    let engineerIds;
+    let allUsers;
     // if (req.user.role !== 'Manager') return Response.authorizationError(res, 'You do not have access to perform this action');
 
     // console.log("id ===>", req.user.id)
@@ -50,7 +60,12 @@ class UserController {
     if (results[0]) {
       engineerIds = results[0].dataValues.engineers;
       console.log('Engineers', engineerIds);
-      if (engineerIds[0]) { allUsers = await findAllUsers({ id: { [Op.or]: engineerIds }, role: 'Trainee' }); }
+      if (engineerIds[0]) {
+        allUsers = await findAllUsers({
+          id: { [Op.or]: engineerIds },
+          role: 'Trainee',
+        });
+      }
 
       return Response.customResponse(res, 200, 'success', allUsers);
     }
@@ -74,10 +89,14 @@ class UserController {
 
   static async viewSingleProfile(req, res) {
     const { id } = req.params;
-    if (isNaN(parseInt(id, 10))) Response.badRequestError(res, 'enter a valid user id');
+    if (isNaN(parseInt(id, 10)))
+      Response.badRequestError(res, 'enter a valid user id');
 
     if (req.user.role === 'Trainee' && parseInt(id, 10) !== req.user.id) {
-      return Response.authorizationError(res, 'Don\'t  have previelage to access this end point');
+      return Response.authorizationError(
+        res,
+        "Don't  have previelage to access this end point"
+      );
     }
 
     const user = await getSingleEngineer({ id });
@@ -90,7 +109,12 @@ class UserController {
   static async getMyProfile(req, res) {
     const user = await getSingleEngineer({ id: req.user.id });
     if (!user) Response.notFoundError(res, 'User not found');
-    return Response.customResponse(res, 200, 'Profile retrieved successfully', user);
+    return Response.customResponse(
+      res,
+      200,
+      'Profile retrieved successfully',
+      user
+    );
   }
 
   static async changeCohort(req, res) {
@@ -98,7 +122,7 @@ class UserController {
       const { id, cohort } = req.params;
       await updateUser({ cohort }, { id });
       await updateUser({ program: null }, { id });
-      return Response.customResponse(res, 200, 'trainee\'s cohort changed!');
+      return Response.customResponse(res, 200, "trainee's cohort changed!");
     } catch (error) {
       return Response.serverError(res, error.message);
     }
@@ -108,7 +132,7 @@ class UserController {
     try {
       const { id, program } = req.params;
       await updateUser({ program }, { id });
-      return Response.customResponse(res, 200, 'trainee\'s program changed!');
+      return Response.customResponse(res, 200, "trainee's program changed!");
     } catch (error) {
       return Response.serverError(res, error.message);
     }
