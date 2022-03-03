e076ce821790f7bc46ecfe526e382d466723098e
nkbtemmy <emmanuelnkubito2@gmail.com>
2022-03-03 14:27:59 +0200

    ft(BN)_assign_manager_to_trainee

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -6,30 +6,30 @@ import Response from '../helpers/response';
 
 class EngineerService {
   async createOrRemove(req, res, next) {
-    console.log('in groups')
+    console.log('in groups');
     try {
       const { engineers } = req.body;
       const { user } = req;
-      console.log('in engineers', engineers)
-      console.log('user', user)
-
+      console.log('in engineers', engineers);
+      console.log('user', user);
 
       if (user.role !== 'Manager') return Response.authorizationError(res, 'you have no rights to perform this operation');
-     
+
       const { id } = user;
       await Promise.all(
         engineers.map(async (engineerId) => {
-          console.log("engineer",engineerId)
+          console.log('engineer', engineerId);
           const engineer = await userService.find({ id: engineerId });
           if (!engineer) {
             return Response.notFoundError(res, `engineer ${engineerId} is not found`);
           }
+          await userService.updateUser({ managerId: id }, { id: engineerId });
         }),
       );
       const result = await engineerService.createOrUpdateGroup(id, engineers);
       return Response.customResponse(res, 200, 'operation performed successfully', result);
     } catch (error) {
-       console.log("Error", error)
+      console.log('Error', error);
       return next(error);
     }
   }
